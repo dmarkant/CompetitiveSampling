42c3c504c213869ed8629f82d0349d7b2b61e143
Doug Markant <dmarkant@gmail.com>
2014-01-29 11:24:16 +0100

    basic experiment structure working

--- exp/compsamp-v1/static/js/task.js
+++ exp/compsamp-v1/static/js/task.js
@@ -19,8 +19,8 @@ psiTurk.preloadPages(pages);
 
 
 // Task object to keep track of the current phase
-var currentview;
-
+var exp,
+	NROUNDS = 6;
 
 
 var sample_from_discrete = function(option) {
@@ -89,12 +89,15 @@ var Option = function(stage, option_info, callback) {
 	self.color = option_info.color;
 	self.x = option_info.x;
 	self.y = option_info.y;
+	self.sample_x = self.x;
+	self.sample_y = self.y + 200;
 	self.stage = stage;
 	self.selection_callback = callback;
 	
 	self.draw = function() {
 		
 		self.disp = self.stage.append('g').attr('id', self.id);
+
 		self.obj = self.disp.append('image')
 							.attr('x', self.x-100)
 							.attr('y', self.y-80)
@@ -102,27 +105,66 @@ var Option = function(stage, option_info, callback) {
 							.attr('height', 200)
 							.attr('xlink:href', 'static/images/pot.png');
 
-		self.circle = self.disp.append('circle')
-						 .attr('cx', self.x)
-						 .attr('cy', self.y)
-						 .attr('r', 10)
-						 .style('fill', 'black')
-						 .style('stroke', self.color)
-						 .style('stroke-width', 3);
+		self.label = self.disp.append('text')
+							  .attr('x', self.x)
+							  .attr('y', self.y+60)
+							  .attr('text-anchor', 'middle')
+							  .attr('class', 'optionlabel')
+							  .attr('stroke', 'gray')
+							  .text(self.id);
+
 		return self;
 	};
 
 	self.highlight = function() {
-		self.circle.style('fill', 'gray');
+
+		self.highlight = self.disp.append('rect')
+								  .attr("rx", 6)
+								  .attr("ry", 6)
+								  .attr("x", self.x-120)
+								  .attr("y", self.y-170)
+								  .attr("width", 240)
+								  .attr("height", 300)
+								  .attr('stroke', 'gray')
+								  .attr('stroke-width', 3)
+								  .attr('fill', 'none');
+
 		return self;
 	};
 
+	self.draw_sample = function(value) {
+
+		self.coin = self.disp.append('g').attr('id', 'coin');
+
+		self.coin.append('image')
+				 .attr('x', self.sample_x-60)
+				 .attr('y', self.sample_y-60)
+				 .attr('width', 120)
+				 .attr('height', 120)
+				 .attr('xlink:href', 'static/images/coin.jpg');
+
+		self.coin.append('text')
+				 .attr('x', self.sample_x)
+				 .attr('y', self.sample_y+15)
+				 .attr('text-anchor', 'middle')
+				 .attr('class', 'samplefeedback')
+				 .text(value);
+	};
+
+	self.clear_sample = function() {
+		self.coin.remove();
+	};
+
 	self.listen = function() {
-		self.circle.on('mousedown', function() {
-			self.highlight();
+		self.disp.on('mousedown', function() {
+			//self.highlight();
 			self.selection_callback(self.id);
 		});
 		return self;
+	};
+
+	self.stop_listening = function() {
+		self.disp.on('mousedown', function() {} );
 	};
 
 	self.erase = function() {
@@ -133,178 +175,205 @@ var Option = function(stage, option_info, callback) {
 };
 
 
-var IndividualSamplingGame = function(gamble_info) {
+var IndividualSamplingGame = function(round, gamble_info) {
 
 	var self = this;
+	self.round = round;
 	self.gamble = gamble_info;
 
-
-	self.do_next_trial = function() {
+	self.reset_stage = function(callback) {
 		psiTurk.showPage('stage.html');
-		console.log('starting next trial');
-
 		self.stage = d3.select("#stagesvg");
 		self.above_stage = d3.select("#aboveStage");
 		self.below_stage = d3.select("#belowStage");
+		self.instruction = d3.select("#instruction");
+		self.buttons = d3.select("#buttons");
 		self.stage_w = self.stage.attr("width");
 		self.stage_h = self.stage.attr("height");
-		
-		self.sampling_trial();
+
+		self.above_stage.html('<h1>Round '+(self.round+1)+'/'+NROUNDS+'</h1>');
+
+		callback();
 	}
 
-	// 
+	self.begin = function() {
+
+		self.above_stage.html('');
+		
+		splash = self.stage.append('g');
+
+
+
+
+		splash.append('text')
+				  .attr('x', self.stage_w/2)
+				  .attr('y', self.stage_h/2)
+				  .attr('fill', 'black')
+				  .attr('text-anchor', 'middle')
+				  .attr('class', 'splash-text')
+				  .text('Round '+(self.round+1)+'/'+NROUNDS);
+
+		// continue button
+		self.btn = self.buttons.append('input')
+								   .attr('value', 'Ready to start')
+			    				   .attr('type', 'button')
+								   .attr('height', 100);
+
+		self.btn.on('click', function() {
+			self.reset_stage(self.sampling_trial);
+		});
+
+	}
+
 	self.sampling_trial = function() {
 
-		self.optionA = new Option(self.stage,
+		self.options = {'A': new Option(self.stage,
 								  {'id': 'A',
 								   'color': 'red',
 								   'x': self.stage_w/4,
-								   'y': self.stage_h/2},
-								  self.generate_sample).draw().listen();
-
-
-		self.optionB = new Option(self.stage,
+								   'y': self.stage_h/3},
+								  self.generate_sample).draw().listen(),
+						'B': new Option(self.stage,
 								  {'id': 'B',
 								   'color': 'blue',
 								   'x': 3 * self.stage_w/4,
-								   'y': self.stage_h/2},
-								  self.generate_sample).draw().listen();
-		
-		// person makes a selection
-
-		// then they get feedback about 
-
-
-	}
-
-	self.prompt_stop_or_continue = function() {
-
-	}
-
-	self.prompt_sample = function() {
+								   'y': self.stage_h/3},
+								  self.generate_sample).draw().listen()};
+	
+		self.instruction.html('Click the option you want to sample.');
 
 	}
 
 	self.generate_sample = function(chosen_id) {
-		console.log('chose ' + chosen_id);
+
+		self.options['A'].stop_listening();
+		self.options['B'].stop_listening();
 
 		result = sample_from_discrete(self.gamble.options[chosen_id]);
-		console.log(result);
 
 		// show feedback
-		self.feedback_disp = self.stage.append('g').attr('id', 'feedback');
-		self.feedback_disp.append('text')
-						  .attr('id', 'sample')
-						  .attr('x', self.stage_w/2)
-						  .attr('y', 2 * self.stage_h/3)
-						  .attr('fill', 'white')
-						  .attr('text-anchor', 'middle')
-						  .attr('class', 'samplefeedback')
-						  .text(result);
+		self.options[chosen_id].draw_sample(result);
+
+		self.instruction.html('You chose option '+chosen_id+' and got a coin worth $'+result+'.');
+
+		// continue button
+		self.btn = self.buttons.append('input')
+								   .attr('value', 'OK')
+			    				   .attr('type', 'button')
+								   .attr('height', 100);
+
+		self.btn.on('click', function() {
+			self.options[chosen_id].clear_sample();	
+			self.prompt_stop_or_continue();
+		});
+	}
+	
+	self.prompt_stop_or_continue = function() {
+
+		self.btn.remove();
+			
+		// continue button
+		continue_btn = self.buttons.append('input')
+								   .attr('value', 'Continue sampling')
+			    				   .attr('type', 'button')
+								   .attr('height', 100);
+
+		continue_btn.on('click', function() {
+			// output choice
+
+			self.reset_stage(self.sampling_trial);
+		});
+
+		// continue button
+		stop_btn = self.buttons.append('input')
+								   .attr('value', 'Stop and choose')
+			    				   .attr('type', 'button')
+								   .attr('height', 100);
+		
+		stop_btn.on('click', function() {
+			// output choice
+
+			self.reset_stage(self.final_decision);
+		});
+		
+		self.instruction.html('Do you want to continue sampling or stop and choose one of the options?');
 
 	}
 
-	self.show_feedback = function() {
+	self.final_decision = function() {
+		console.log('at the final decision');
+
+		self.options = {'A': new Option(self.stage,
+								  {'id': 'A',
+								   'color': 'red',
+								   'x': self.stage_w/4,
+								   'y': self.stage_h/3},
+								  self.show_feedback).draw().listen(),
+						'B': new Option(self.stage,
+								  {'id': 'B',
+								   'color': 'blue',
+								   'x': 3 * self.stage_w/4,
+								   'y': self.stage_h/3},
+								  self.show_feedback).draw().listen()};
+
+		self.instruction.html('Click on the option you want!');
+		
 
 	}
 
+	self.show_feedback = function(chosen_id) {
+		console.log('show feedback');
+
+		// continue button
+		self.btn = self.below_stage.append('input')
+								   .attr('value', 'OK')
+			    				   .attr('type', 'button')
+								   .attr('height', 100);
+
+		self.btn.on('click', function() {
+			self.finish();
+		});
+
+		self.instruction.html('You chose option '+chosen_id+'. Your earnings from this choice will be shown at the end of the experiment.');
+		
+
+	}
+
+	self.finish = function() {
+		console.log('finished this round');
+		exp.next();
+	}
 
 
-	self.do_next_trial();
+	self.reset_stage(self.begin);
 	return self;
 };
 
 
+var IndividualSamplingExperiment = function() {
 
+	self = this;
+	self.round = -1;
 
-
-/********************
-* STROOP TEST       *
-********************/
-
-var TestPhase = function() {
-
-	var wordon, // time word is presented
-	    listening = false,
-	    resp_prompt = '<p id="prompt">Type<br> "R" for Red<br>"B" for blue<br>"G" for green.';
-	
-	var next = function() {
-		if (stims.length===0) {
-			finish();
-		}
-		else {
-			stim = stims.shift();
-			show_word( stim[0], stim[1] );
-			wordon = new Date().getTime();
-			listening = true;
-			$('#query').html(resp_prompt).show();
-		}
+	self.gamble = {'options': {'A': {'H': 50, 'L': -20, 'p': .5},
+							   'B': {'H': 40, 'L': -30, 'p': .2}}
 	};
-	
-	var response_handler = function(e) {
-		if (!listening) return;
 
-		var keyCode = e.keyCode,
-			response;
 
-		switch (keyCode) {
-			case 82:
-				// "R"
-				response="red";
-				break;
-			case 71:
-				// "G"
-				response="green";
-				break;
-			case 66:
-				// "B"
-				response="blue";
-				break;
-			default:
-				response = "";
-				break;
-		}
-		if (response.length>0) {
-			listening = false;
-			var hit = response == stim[1];
-			var rt = new Date().getTime() - wordon;
+	self.next = function() {
+		self.round += 1;
 
-			psiTurk.recordTrialData(["TEST", stim[0], stim[1], stim[2], response, hit, rt]);
+		if (self.round < NROUNDS) {
 			
-			remove_word();
-			next();
-		}
+			self.view = new IndividualSamplingGame(self.round, self.gamble);
+
+		};
 	};
 
-	var finish = function() {
-		$("body").unbind("keydown", response_handler); // Unbind keys
-		currentview = new Questionnaire();
-	};
+	self.next();
 	
-	
-	// Load the test.html snippet into the body of the page
-	psiTurk.showPage('test.html');
-	
-	// This uses the Raphael library to create the stimulus. Note that when
-	// this is created the first argument is the id of an element in the
-	// HTML page (a div with id 'stim')
-	var R = Raphael("stim", 500, 200),
-		font = "100px Helvetica";
-	
-	var show_word = function(text, color) {
-		R.text( 250, 100, text ).attr({font: font, fill: color});
-	};
-	var remove_word = function(text, color) {
-		R.clear();
-	};
-
-	// Register the response handler that is defined above to handle any
-	// key down events.
-	$("body").focus().keydown(response_handler); 
-
-	// Start the test
-	next();
 };
+
 
 
 /****************
@@ -373,16 +442,7 @@ var completeHIT = function() {
  * Run Task
  ******************/
 $(window).load( function(){
-
-	var gamble = {'options': {'A': {'H': 50, 'L': -20, 'p': .5},
-							  'B': {'H': 40, 'L': -30, 'p': .2}}
-	};
-
-
-    //currentview = new Instructions([
-	//	"instruct.html"
-	//]);
-	currentview = new IndividualSamplingGame(gamble);
+	exp = new IndividualSamplingExperiment();
 });
 
 // vi: noexpandtab tabstop=4 shiftwidth=4
